e552022929beff4d65734b41f0fb2b686293db74
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-04-26 20:57:46 +0000

    sdm710-common: Derp-ify
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
 
+# Remove Google Recorder
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Context Hub
 PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-impl.generic \
